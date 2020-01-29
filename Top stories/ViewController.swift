e710f7d4176faac42b9dff2f2b6ccfa858d6fb03
Jack <jkafka@bsd220.org>
2020-01-29 10:19:24 -0600

    Initial Commit

---
+++ Top stories/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Top stories
+//
+//  Created by Jack Kafka on 1/29/20.
+//  Copyright © 2020 Jack Kafka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
